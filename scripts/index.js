0c545094c13b4c2ab7e2a5f981928f9359821c7e
konstantinsmirnov <smirnov.konstantin.93@gmail.com>
2023-05-01 22:08:06 +0600

    feat: caps functionality

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ const keyBoard = [
     {"enMode": "-", "enShitMode": "_", "ruMode": "-", "ruShiftMode": "_", "size": "small", "row": 1, "color": "grey"},
     {"enMode": "=", "enShitMode": "+", "ruMode": "=", "ruShiftMode": "+", "size": "small", "row": 1, "color": "grey"},
     {"enMode": "Backspace", "enShitMode": "Backspace", "ruMode": "Backspace", "ruShiftMode": "Backspace", "size": "large", "row": 1, "color": "black"},
-    
+
     {"enMode": "Tab", "enShitMode": "Tab", "ruMode": "Tab", "ruShiftMode": "Tab", "size": "large", "row": 2, "color": "black"},
     {"enMode": "q",   "enShitMode": "Q",   "ruMode": "й",   "ruShiftMode": "Й",   "size": "small", "row": 2, "color": "grey"},
     {"enMode": "w",   "enShitMode": "W",   "ruMode": "ц",   "ruShiftMode": "Ц",   "size": "small", "row": 2, "color": "grey"},
@@ -44,7 +44,7 @@ const keyBoard = [
     {"enMode": "'",   "enShitMode": '"',   "ruMode": "э",   "ruShiftMode": "Э",   "size": "small", "row": 3, "color": "grey"},
     {"enMode": "Enter", "enShitMode": "Enter", "ruMode": "Enter", "ruShiftMode": "Enter", "size": "large", "row": 3, "color": "black"},
 
-    {"enMode": "Shift", "enShitMode": "CapsLock", "ruMode": "CapsLock", "ruShiftMode": "CapsLock", "size": "large", "row": 4, "color": "black"},
+    {"enMode": "Shift", "enShitMode": "Shift", "ruMode": "Shift", "ruShiftMode": "Shift", "size": "large", "row": 4, "color": "black"},
     {"enMode": "z",   "enShitMode": "Z",   "ruMode": "я",   "ruShiftMode": "Я",   "size": "small", "row": 4, "color": "grey"},
     {"enMode": "x",   "enShitMode": "X",   "ruMode": "ч",   "ruShiftMode": "Ч",   "size": "small", "row": 4, "color": "grey"},
     {"enMode": "c",   "enShitMode": "C",   "ruMode": "с",   "ruShiftMode": "С",   "size": "small", "row": 4, "color": "grey"},
@@ -56,7 +56,7 @@ const keyBoard = [
     {"enMode": ".",   "enShitMode": ">",   "ruMode": "ю",   "ruShiftMode": "Ю",   "size": "small", "row": 4, "color": "grey"},
     {"enMode": "/",   "enShitMode": "?",   "ruMode": ".",   "ruShiftMode": ",",   "size": "small", "row": 4, "color": "grey"},
     {"enMode": "▲",   "enShitMode": '▲',   "ruMode": "▲",   "ruShiftMode": "▲",   "size": "small", "row": 4, "color": "black"},
-    {"enMode": "Shift", "enShitMode": "Enter", "ruMode": "Enter", "ruShiftMode": "Enter", "size": "large", "row": 4, "color": "black"},
+    {"enMode": "Shift", "enShitMode": "Shift", "ruMode": "Shift", "ruShiftMode": "Shift", "size": "large", "row": 4, "color": "black"},
 
     {"enMode": "Ctr", "enShitMode": "Ctr", "ruMode": "Ctr", "ruShiftMode": "Ctr", "size": "large",    "row": 5, "color": "black"},
     {"enMode": "Win", "enShitMode": "Win", "ruMode": "Win", "ruShiftMode": "Win", "size": "large",    "row": 5, "color": "black"},
@@ -89,18 +89,18 @@ function init() {
 
         keyBoardStr += `<div class="keyboard__item">
                           <span class="keyboard__item_en">
-                            <span class="item ${item.size} ${item.color}">
+                            <span class="item letter-mode ${item.size} ${item.color}">
                               ${item.enMode}
                             </span>
-                            <span class="item shift-mode hidden">
+                            <span class="item ${item.size} ${item.color} shift-mode hidden">
                               ${item.enShitMode}
                             </span>
                           </span>
                           <span class="keyboard__item_ru hidden">
-                            <span class="item hidden">
+                            <span class="item hidden ${item.size} ${item.color}">
                               ${item.ruMode}
                             </span>
-                            <span class="item shift-mode hidden">
+                            <span class="item shift-mode hidden ${item.size} ${item.color}">
                               ${item.ruShiftMode}
                             </span>
                           </span>
@@ -139,10 +139,18 @@ function mouseKeyDown(event) {
 
 function addMouseEvent() {
     const keyElements = document.querySelectorAll("span.item");
+
     keyElements.forEach(item => {
         item.addEventListener("mouseup", mouseKeyPress);
         item.addEventListener("mousedown", mouseKeyDown);
+        document.querySelector("textarea").focus();
+        document.querySelector("textarea").addEventListener("keyup", keyPress);
     });
+
+}
+
+function keyPress(event) {
+    console.log(event);
 }
 
 function checkKeyValue(element) {
@@ -150,12 +158,17 @@ function checkKeyValue(element) {
     if (element.innerHTML.trim() == "Tab") {
         insertText(textarea, "    ");
     } else if (element.innerHTML.trim() == "Backspace" && textarea.value.length > 0) {
-        textarea.value = textarea.value.slice(0, textarea.value.length - 1);
+        insertText(textarea, element.innerHTML.trim());
+    } else if (element.innerHTML.trim() == "▼" || element.innerHTML.trim() == "►" ||
+               element.innerHTML.trim() == "◄" || element.innerHTML.trim() == "▲") {
+        insertTextArrows(textarea, element.innerHTML.trim());
+    } else if (element.innerHTML.trim() == "CapsLock") {
+        chageKeyBoardToUpperCase();
     } else if (element.innerHTML.trim() != "Ctr"         && element.innerHTML.trim() != "Alt" &&
-               element.innerHTML.trim() != "CapsLock"    && element.innerHTML.trim() != "Win" &&
+               element.innerHTML.trim() != "Win" &&
                element.innerHTML.trim() != "Shift"       && element.innerHTML.trim() != "Del" &&
-               element.innerHTML.trim() != "Enter"       &&
-               element.innerHTML.trim() != "" && element.innerHTML.trim() != "Backspace")
+               element.innerHTML.trim() != "Enter"       && element.innerHTML.trim() != ""    &&
+               element.innerHTML.trim() != "Backspace")
     {
         insertText(textarea, element.innerHTML.trim());
     }
@@ -163,14 +176,43 @@ function checkKeyValue(element) {
 
 function insertText(textarea, text) {
     let endIndex;
+    textarea.focus();
 
-    if (typeof textarea.selectionStart == "number"
-            && typeof textarea.selectionEnd == "number") {
+    if (typeof textarea.selectionStart == "number" && typeof textarea.selectionEnd == "number") {
         endIndex = textarea.selectionEnd;
-        textarea.value = textarea.value.slice(0, endIndex) + text + textarea.value.slice(endIndex);
-        textarea.selectionStart = textarea.selectionEnd = endIndex + text.length;
+        
+        if (text == 'Backspace') {
+            textarea.value = textarea.value.slice(0, textarea.selectionStart - 1) + textarea.value.substring(textarea.selectionStart);
+            textarea.selectionEnd = textarea.selectionStart;
+        } else {
+            textarea.value = textarea.value.slice(0, endIndex) + text + textarea.value.slice(endIndex);
+            textarea.selectionStart = textarea.selectionEnd = endIndex + text.length;
+        }
+
     }
 }
 
+function insertTextArrows(textarea, text) {
+    textarea.focus();
+    if (text == '◄' && textarea.selectionStart != 0) {
+        textarea.selectionEnd -= 1;
+    } else if (text == '►' && textarea.value.length > 0) {
+        textarea.selectionStart += 1;
+    }
+}
+
+function chageKeyBoardToUpperCase() {
+    const allEnKey = document.querySelectorAll('.keyboard__item_en');
+
+    allEnKey.forEach(item => {
+        item.querySelector('.letter-mode').classList.toggle('hidden');
+        item.querySelector('.shift-mode').classList.toggle('hidden');
+
+        if (item.querySelector('.letter-mode').innerHTML.trim() == 'CapsLock' || item.querySelector('.shift-mode').innerHTML.trim() == 'CapsLock') {
+            item.querySelector('.letter-mode').classList.toggle('toggle');
+            item.querySelector('.shift-mode').classList.toggle('toggle');
+        }
+    });
+}
 
 init();
